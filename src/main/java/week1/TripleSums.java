73b01e8fa643891e53c0360a7c3b909cbb420d65
miguel5 <miguel.amigot@gmail.com>
2014-06-13 13:43:09 -0400

    start

---
+++ src/main/java/week1/TripleSums.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class TripleSums {
+
+	private static int[] genArrayInts(){
+		
+		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] ints = genArrayInts();
+
+	}
+
+}
