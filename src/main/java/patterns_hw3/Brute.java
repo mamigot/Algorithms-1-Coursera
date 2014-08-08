c2d639b8fe35416c2cbe89daff71d02f9f74b068
miguel5 <miguel.amigot@gmail.com>
2014-08-08 04:06:09 +0200

    setup of function calls... should still add a private line class though

--- src/main/java/patterns_hw3/Brute.java
+++ src/main/java/patterns_hw3/Brute.java
@@ -28,30 +28,56 @@ public class Brute {
 		return points;
 	}
 
-	private static boolean isSameSegment(List<Point> points) {
+	private static boolean areCollinear(List<Point> points) {
 		// Checks if all of the provided points are on the same segment.
 		// To check whether the 4 points p, q, r, and s are collinear, check
 		// whether the slopes between p and q, between p and r, and between p
 		// and s are all equal.
+		return true;
 
-		assert (points.size() == 4);
+	}
 
-		// Only need to compare one point against all of the others
-		Point chosenPoint = points.get(0);
-		double establishedSlope = chosenPoint.slopeTo(points.get(1));
+	private static List<List<Point>> assembleDistinctLineSegments(
+			List<Point> readPoints) {
 
-		Point currPoint;
-		double currSlope;
-		int size = points.size();
-		for (int i = 2; i < size; i++) {
-			currPoint = points.get(i);
-			currSlope = chosenPoint.slopeTo(currPoint);
+		List<List<Point>> lineSegments = new LinkedList<List<Point>>();
 
-			if (currSlope != establishedSlope)
-				return false;
+		List<Point> currList = new LinkedList<Point>();
+		boolean collinear;
+		for (Point p1 : readPoints) {
+			currList.clear();
+
+			// Create a sorted list of the points
+			currList.add(p1);
+
+			for (Point p2 : readPoints) {
+				currList.add(p2);
+
+				for (Point p3 : readPoints) {
+					currList.add(p3);
+
+					for (Point p4 : readPoints) {
+						currList.add(p4);
+
+						// Check if the points are collinear
+						collinear = areCollinear(currList);
+
+						// Add to lineSegments if the
+						// permutation doesn't exist already
+						if (collinear) {
+							// Check if it's in lineSegments and add accordingly
+							// DOUBLE CHECK THAT THIS "CONTAINS" WORKS
+							if (!lineSegments.contains(currList))
+								lineSegments.add(currList);
+
+						}
+
+					}
+				}
+			}
 		}
 
-		return true;
+		return lineSegments;
 	}
 
 	public static void main(String[] args) {
@@ -64,9 +90,11 @@ public class Brute {
 
 		}
 
-		List<Point> points = readPoints(filename);
+		List<Point> readPoints = readPoints(filename);
 
-		
+		List<List<Point>> lines = assembleDistinctLineSegments(readPoints);
+
+		// Display and print the line segments
 
 	}
 
